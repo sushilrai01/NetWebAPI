9e875f35a8ea4218e4f25c5ae9274dcf7d6ba816
sushilrai01 <ssilray16@gmail.com>
2023-09-22 12:02:55 +0545

    Consume Post method of Web API to execute HTTP POST request.

---
+++ Controllers/DriverAPIController.cs
@@ -0,0 +1,62 @@
+﻿using DriverInformation.ViewModel;
+using NetWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NetWebAPI.Controllers
+{
+    public class DriverAPIController : ApiController
+    {
+        private DriverManagementEntities db = new DriverManagementEntities();
+
+        public IHttpActionResult GetAllDriver()
+        {
+            IList<DriverInfoModel> driverInfo = null;
+            driverInfo = db.DriverTable.Select(x => new DriverInfoModel()
+            {
+                DriverId = x.DriverId,
+                DriverName = x.Name,
+                ContactNo = x.ContactNo,    
+                GenderId = x.GenderId == null? 0: x.GenderId.Value,
+                ActiveId = x.IsActive == null ? 0 : x.IsActive.Value,
+                Football = x.Football ==null ? false: x.Football.Value, 
+            }).ToList();
+
+            if(driverInfo.Count() == 0)
+            {
+                return NotFound();  
+            }
+
+            return Ok(driverInfo);
+        }
+
+        public IHttpActionResult PostNewDriver(DriverInfoModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");  
+            }
+
+            DriverTable drivertbl = new DriverTable();
+
+            drivertbl.DriverId = model.DriverId;
+            drivertbl.Name = model.DriverName;
+            drivertbl.ContactNo = model.ContactNo;      
+            drivertbl.Football = model.Football;
+            drivertbl.GenderId = model.GenderId;
+            drivertbl.IsActive = model.ActiveId;
+            drivertbl.Basketball = model.Basketball;    
+            drivertbl.Cricket = model.Cricket;
+
+            db.DriverTable.Add(drivertbl);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+    } //<--MAIN DRIVER-CONTROLLER CLASS.
+}
--- Controllers/DriverController.cs
+++ Controllers/DriverController.cs
@@ -3,37 +3,63 @@ using NetWebAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 
 namespace NetWebAPI.Controllers
 {
-    public class DriverController : ApiController
+    public class DriverController : Controller
     {
         private DriverManagementEntities db = new DriverManagementEntities();
 
-        public IHttpActionResult GetAllDriver()
+        // GET: Driver
+        public ActionResult Index()
         {
-            IList<DriverInfoModel> driverInfo = null;
-            driverInfo = db.DriverTable.Select(x => new DriverInfoModel()
-            {
-                DriverId = x.DriverId,
-                DriverName = x.Name,
-                ContactNo = x.ContactNo,    
-                GenderId = x.GenderId == null? 0: x.GenderId.Value,
-                ActiveId = x.IsActive == null ? 0 : x.IsActive.Value,
-                Football = x.Football ==null ? false: x.Football.Value, 
-            }).ToList();
+            return View();
+        }
+        //GET: Driver/Create
+        public ActionResult Create()
+        {
+            DriverInfoModel model = new DriverInfoModel();
 
-            if(driverInfo.Count() == 0)
-            {
-                return NotFound();  
-            }
+            model.GenList = db.GenderTable
+                               .Select(x => new DropdownModel { ID = x.GenderId, TEXT = x.Category }).ToList();
+            model.ActList = db.ActivityTable
+                              .Select(x => new DropdownModel { ID = x.IsActive, TEXT = x.Available }).ToList();
+            model.HobList = db.HobbyTable
+                                 .Select(x => new HobbyModel { HobbyId = x.HobbyId, Hobby = x.Hobby, IsActive = x.IsActive == null ? false : x.IsActive.Value }).ToList();
 
-            return Ok(driverInfo);
+            return View(model);
         }
 
+        [HttpPost]
+        public ActionResult Create(DriverInfoModel model)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44348/api/DriverAPI");
 
-    } //<--MAIN DRIVER-CONTROLLER CLASS.
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<DriverInfoModel>("DriverAPI", model);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+            model.GenList = db.GenderTable
+                             .Select(x => new DropdownModel { ID = x.GenderId, TEXT = x.Category }).ToList();
+            model.ActList = db.ActivityTable
+                              .Select(x => new DropdownModel { ID = x.IsActive, TEXT = x.Available }).ToList();
+            model.HobList = db.HobbyTable
+                                 .Select(x => new HobbyModel { HobbyId = x.HobbyId, Hobby = x.Hobby, IsActive = x.IsActive == null ? false : x.IsActive.Value }).ToList();
+
+            return View(model);
+        }
+    }
 }
